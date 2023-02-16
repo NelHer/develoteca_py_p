0362522cbaa9f58a700fb9e7c78d5ffb2f3b38a1
nelsonherrera <nahb.personal@gmail.com>
2023-02-15 21:18:07 -0400

    Se agrego un archivo para los archivos js

---
+++ js/jsAddTela.js
@@ -0,0 +1,32 @@
+function toggleFields() {
+    // Obtener todos los elementos con id 'sectionNombreTela', 'sectionPrecioTela', etc.
+    const sectionNombreTela = document.getElementById("sectionNombreTela");
+    const sectionPrecioTela = document.getElementById("sectionPrecioTela");
+    const sectionMaterialesTela = document.getElementById("sectionMaterialesTela");
+    const sectionEstiloTela = document.getElementById("sectionEstiloTela");
+    const sectionAnchoTela = document.getElementById("sectionAnchoTela");
+    const sectionColoresTela = document.getElementById("sectionColoresTela");
+    const sectionNotasTela = document.getElementById("sectionNotasTela");
+    
+    // Mostrar o ocultar secciones en función de la selección del usuario
+    if (document.getElementById("fotoTela").value) {
+      sectionNombreTela.style.display = "block";
+      sectionPrecioTela.style.display = "block";
+      sectionMaterialesTela.style.display = "block";
+      sectionEstiloTela.style.display = "block";
+      sectionAnchoTela.style.display = "block";
+      sectionColoresTela.style.display = "block";
+      sectionNotasTela.style.display = "block";
+    } else {
+      sectionNombreTela.style.display = "none";
+      sectionPrecioTela.style.display = "none";
+      sectionMaterialesTela.style.display = "none";
+      sectionEstiloTela.style.display = "none";
+      sectionAnchoTela.style.display = "none";
+      sectionColoresTela.style.display = "none";
+      sectionNotasTela.style.display = "none";
+    }
+  }
+
+  // Agregar un controlador de eventos al elemento con id "fotoTela"
+  document.getElementById("fotoTela").addEventListener("change", toggleFields);
